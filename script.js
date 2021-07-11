add8474ea345199a9c2f87f09aed590f4dbe42f4
jahedurrahman125 <jahed0003@gmail.com>
2021-07-11 20:57:28 +0300

    click event add for user input marker save

--- script.js
+++ script.js
@@ -7,12 +7,10 @@ function initMap() {
         lng: 24.857580172807722
     },
     zoom: 16,
+    mapId: '814bf60920bc7d90'
   });
   infoWindow = new google.maps.InfoWindow();
-  const locationButton = document.createElement("button");
-  locationButton.textContent = "Location Me";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  const locationButton = document.getElementById("LocateMe");
   locationButton.addEventListener("click", () => {
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -22,12 +20,29 @@ function initMap() {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
+          console.log("pos",pos)
           new google.maps.Marker({
             position: pos,
+            
             map,
             title: "user!",
+            icon:{
+                url:"/markerImg/you-are-here.svg",
+                scaledSize: new google.maps.Size(31, 32)
+            }
           });
         
+          // Configure the click listener.
+            let newPos;
+            map.addListener("click", (mapsMouseEvent, pos) => {
+                position: mapsMouseEvent.latLng,
+                newPos = JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)  
+            new google.maps.Marker({
+                position: JSON.parse(newPos), 
+                map,
+                
+            });
+         })
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter());
@@ -38,6 +53,7 @@ function initMap() {
       handleLocationError(false, infoWindow, map.getCenter());
     }
   });
+ 
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
